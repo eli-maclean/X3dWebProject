39a570a634998eb060960ee99e589982e922fab3
lf03747 <60618637+lf03747@users.noreply.github.com>
2020-04-18 18:26:50 -0400

    Model types
    with new Css

---
+++ basic.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+?>
+<html>
+	<head>
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.7.1.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.js"></script>
+		<link rel='stylesheet' type='text/css' href='modelpages/css/main.css'/>
+		<title>Basic Models</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	.cell {
+					display:block;
+					float: left;
+					width: 28%;
+			background: #ffffff;
+			margin: 15px 15px 30px 30px;
+			border: #E0E0E0 1px solid;
+			
+
+		 }
+		 .thumbnail{
+			height: 155px;
+			width: 250px;
+			background-color: #FFF;
+
+			border:1px solid black;
+			
+			
+		}
+		.title {
+			padding: 15px 15px 0px 15px;
+			overflow: auto;
+			text-align: center;
+			background-color: #D4D4D4; 
+			border:1px solid black;
+		}
+		 
+	</style>	
+	</head>
+		<body>
+	<center>
+		<table class=mpg><tr><td style="border: 0px solid #FFFFFF;">
+			<center>
+			<table class="title" cellpadding="20">
+				<tr><td>
+					<center><h1>Which Basic 3D Model Would You Like To View?</h1></center>
+				</td></tr>
+				<tr>
+					<td><center>
+							<table class=wrt>
+								<tr>
+									<td class=wrs><a class=wrt href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/modelspage.php">Models</a></td>
+									<td class=wrt><a class=wrt href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/decomposable.php">Decomposable</a></td>
+									<td class=wrt><a class=wrt href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/volume.php">Volume</a></td>
+								</tr>
+							</table>
+					</td></center>
+				</tr>
+			</table>
+			
+		    <center>
+				<h2>Model Types</h2>
+			<hr>
+		<div class="allcategories"><?php
+			$product_array = $db_handle->runQuery("SELECT * FROM models");
+			if (!empty($product_array)) {
+				foreach($product_array as $key=>$value){
+					if ($product_array [$key]["type"]=="basic"){
+			?>
+				<div class="cell">
+					<div class="thumbnail"><a href="<?php echo $product_array[$key]["adr"]; ?>"><img src="<?php echo $product_array[$key]["thumbnail"]; ?>"></a></div>
+					<div class="title"><?php echo $product_array[$key]["name"]; ?></div>
+				</div>
+			<?php
+					}
+				}
+			}
+			?>
+		</div>
+	</body>
+</html>
---
+++ modelspage.php
@@ -0,0 +1,55 @@
+<html>
+	
+	<head>
+		<title>X3D Model types</title>
+		<link rel='stylesheet' type='text/css' href='modelpages/css/main.css'/>
+	</head>
+	<body>
+	<center>
+		<table class=mpg><tr><td style="border: 0px solid #FFFFFF;">
+			<center>
+			<table class="title" cellpadding="20">
+				<tr><td>
+					<center><h1>Which type of 3D Model Would You Like To View?</h1></center>
+				</td></tr>
+				<tr>
+					<td><center>
+							<table class=wrt>
+								<tr>
+									<td class=wrs><a class=wrt href=introduction.html>Introduction</a></td>
+									<td class=wrt><a class=wrt href=sensor.html>Why X3D?</a></td>
+									<td class=wrt><a class=wrt href=x3d.html>Models</a></td>
+									<td class=wrt><a class=wrt href=realtime.html>Manual</a></td>
+									<td class=wrt><a class=wrt href=contact.html>Contact</a></td>
+								</tr>
+							</table>
+					</td></center>
+				</tr>
+			</table>
+			
+		    <center>
+				<h2>Model Types</h2>
+			<hr>
+			<table style="width:65%"> 
+				
+				<table style="width:80%"> 
+                <tr><th><a href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/basic.php"><img src="thumbnails/Basic.jpg" style="width:350px;height:300px;"></a></th>
+                    <th><a href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/decomposable.php"><img src="thumbnails/Decomposable.jpg" style="width:350px;height:300px;"></a></th>
+					<th><a href="http://web-students.armstrong.edu/~lf03747/X3dWebProject-master-test/volume.php"><img src="thumbnails/Volume.jpg" style="width:350px;height:300px;"></a></th>
+				</tr>
+                <br/>
+                <br/>
+                <tr><th>Basic</th>
+                    <th>Decomposible</th>
+					<th>Volume</th>
+                </tr>
+				</table>
+			</center>
+			<br/>
+			<center><table class=title><tr><td style="border: 0px solid #FFFFFF;">
+			<center><i><div style="margin-bottom: 12px">@ 2020 Prof. Felix G. Hamza-Lup</i><img style="position: relative; top: 6.5px;" src=gslo.png></img></div>
+			</center></td></tr></table>
+			<br/>
+	</td></tr></table></center><br/>
+	</body>
+</html>
